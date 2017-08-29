bbb1e199e47e46e8ca99fbac79618251278c772f
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-08-29 03:56:17 +0000

    Update OutgoingMail table

--- db/migrate/20170829035245_create_outgoing_mails.rb
+++ db/migrate/20170829035245_create_outgoing_mails.rb
@@ -3,10 +3,6 @@ class CreateOutgoingMails < ActiveRecord::Migration[5.1]
     create_table :outgoing_mails do |t|
       t.string :title
       t.string :description
-      t.references :section, index: true #Link to section
-      t.references :user, index: true #Link to user who create it
-      t.references :role, index: true #Link to role
-      t.references :status, index: true #Link to status
 
       t.timestamps
     end
