7cee8b24e1d4dc15a7db957645655740bd1cec83
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-08-30 08:32:53 +0000

    Migrate IncomingMail table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829035245) do
+ActiveRecord::Schema.define(version: 20170830082819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "incoming_mails", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "outgoing_mails", force: :cascade do |t|
     t.string "title"
