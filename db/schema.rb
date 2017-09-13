14e5abfef24ad608937bae89f40b3bb4011744ee
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-09-13 08:55:50 +0000

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904205725) do
+ActiveRecord::Schema.define(version: 20170913084418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "incoming_mails", force: :cascade do |t|
     t.string "title"
