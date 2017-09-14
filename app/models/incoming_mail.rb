12a63900d779d0eed8bbf4aa5a528772af435496
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-09-14 08:11:21 +0000

    Add status to IncomingMail

--- app/models/incoming_mail.rb
+++ app/models/incoming_mail.rb
@@ -1,3 +1,5 @@
 class IncomingMail < ApplicationRecord
+  enum status: [ :konsep, :koreksi_kasie_1, :revisi_kasie_1 ]
+  
   has_many :comments, :as => :commentable
 end
--- db/migrate/20170830082819_create_incoming_mails.rb
+++ db/migrate/20170830082819_create_incoming_mails.rb
@@ -3,6 +3,7 @@ class CreateIncomingMails < ActiveRecord::Migration[5.1]
     create_table :incoming_mails do |t|
       t.string :title
       t.string :description
+      t.column :status, :integer, default: 0
 
       t.timestamps
     end
