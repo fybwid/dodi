84b4d381f5332167dc2849160e4abd6eb38ed2dd
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-09-14 08:16:12 +0000

    Add status to OutgoingMail

--- app/models/outgoing_mail.rb
+++ app/models/outgoing_mail.rb
@@ -1,3 +1,7 @@
 class OutgoingMail < ApplicationRecord
+  enum status: [ :konsep, :koreksi_kasie_1, :revisi_kasie_1, :koreksi_kasie_2, :revisi_kasie_2,
+                  :koreksi_ktu_1, :revisi_ktu_1, :koreksi_ktu_2, :revisi_ktu_2, :koreksi_kepala_1,
+                  :revisi_kepala_1, :koreksi_kepala_2, :revisi_kepala_2, :fix ]
+  
   has_many :comments, :as => :commentable
 end
